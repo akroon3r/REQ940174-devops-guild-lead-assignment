6b6877cef10150484fe41a2d70d4842960889cf4
Adam Krooner <adam.kroon@gov.bc.ca>
2022-12-09 21:28:48 -0800

    typo in deploy helper

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -6,7 +6,7 @@ objects=( $DC_TEMPLATE $SERVICE_TEMPLATE $ROUTE_TEMPLATE )
 
 for i in "${objects[@]}"
 do
-    oc process -f /home/runner/work/REQ940174-devops-guild-lead-assignment/EQ940174-devops-guild-lead-assignment/openshift/templates/$DIR/"$i" --namespace=$NAMESPACE \
+    oc process -f /home/runner/work/REQ940174-devops-guild-lead-assignment/REQ940174-devops-guild-lead-assignment/openshift/templates/$DIR/"$i" --namespace=$NAMESPACE \
         -p APPLICATION_NAME=$APPLICATION_NAME \
         -p LICENSE_PLATE=$LICENSE_PLATE \
         -p IMAGESTREAM=$IMAGESTREAM \
