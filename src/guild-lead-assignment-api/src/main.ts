e6e081bc1982fada7b8a3561de8622b8a155f281
Adam Krooner <adam.kroon@gov.bc.ca>
2022-12-10 13:48:34 -0800

    swagger example implementation

--- src/guild-lead-assignment-api/src/main.ts
+++ src/guild-lead-assignment-api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Guild Lead Assignment API')
+    .setDescription('Guild Lead Assignment API Description')
+    .setVersion('1.0')
+    .addTag('Guild Lead Assignment API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);  
+
   await app.listen(3000);
   console.log('The Guild Lead Assignment API is live and serving traffic on port 3000')
 }
