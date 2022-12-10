69392e9b013a202321d666f7c0d6db2bec334b9f
Adam Krooner <adam.kroon@gov.bc.ca>
2022-12-09 21:17:32 -0800

    adding deploy helper bash script

---
+++ .github/helpers/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# script to process either the DPIA app or api deployment config templates and deploy it to a given namespace
+
+objects=( $DC_TEMPLATE $SERVICE_TEMPLATE $ROUTE_TEMPLATE )
+
+for i in "${objects[@]}"
+do
+    oc process -f /home/runner/work/REQ940174-devops-guild-lead-assignment/EQ940174-devops-guild-lead-assignment/openshift/templates/$DIR/"$i" --namespace=$NAMESPACE \
+        -p APPLICATION_NAME=$APPLICATION_NAME \
+        -p LICENSE_PLATE=$LICENSE_PLATE \
+        -p IMAGESTREAM=$IMAGESTREAM \
+        -p ENVIRONMENT=$ENVIRONMENT | \
+        oc apply -f -
+done
