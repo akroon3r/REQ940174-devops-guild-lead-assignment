1be55222b0a7303dbdd8e06f11ce4af371cb863e
Adam Krooner <adam.kroon@gov.bc.ca>
2022-12-09 19:34:38 -0800

    adding basic GET health endpoint to API

--- src/guild-lead-assignment-api/src/main.ts
+++ src/guild-lead-assignment-api/src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('The Guild Lead Assignment API is live and serving traffic on port 3000')
 }
 bootstrap();
